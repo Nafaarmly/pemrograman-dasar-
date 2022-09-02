b47332c5669609e2f1a920e6721aab21c61681b7
Nafaarmly <111984912+Nafaarmly@users.noreply.github.com>
2022-09-02 08:35:25 +0700

    Add files via upload

---
+++ hello-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Nafa Armelya Nur Ashsyifa"<<endl;
+    cout<<"Kelas X PPLG 1"<<endl;
+   cout<<"========="<<endl; 
+   cout <<"Angka 1=10"<<endl;
+   cout<<"Angka2 =5 "<<endl;
+   cout<<"Hasil :"<<10+5 <<endl;
+}
